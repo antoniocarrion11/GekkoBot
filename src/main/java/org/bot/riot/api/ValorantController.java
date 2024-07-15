d5d11ffc4629e1a6a760d02bbbefbd93cca8f7fd
Tony <antoniocarrion11@gmail.com>
2024-07-15 10:29:25 -0700

    Barebones of springboot and discord app work, going to make two branches, implementing henrik's api and riot api directly

--- src/main/java/org/bot/riot/api/ValorantController.java
+++ src/main/java/org/bot/riot/api/ValorantController.java
@@ -1,8 +1,14 @@
 package org.bot.riot.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ValorantController {
+
+    @GetMapping("/greeting")
+    public String get() {
+        return "Ayo! I'm Gekkobot your guy in the chair!";
+    }
 
 }
