5de40043d8700bda1591b87f0d44deb1b4d4e39b
Tony <antoniocarrion11@gmail.com>
2024-08-14 18:55:55 -0700

    Inconsistent data for some reason. Committing again.

--- src/main/java/org/bot/riot/model/match/AbilityCasts.java
+++ src/main/java/org/bot/riot/model/match/AbilityCasts.java
@@ -6,15 +6,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AbilityCasts {
-    private int grenade;
-    private int ability1;
-    private int ability2;
-    private int ultimate;
+  private int grenade;
+  private int ability1;
+  private int ability2;
+  private int ultimate;
 
-    public AbilityCasts(int grenade, int ability1, int ability2, int ultimate) {
-        this.grenade = grenade;
-        this.ability1 = ability1;
-        this.ability2 = ability2;
-        this.ultimate = ultimate;
-    }
+  public AbilityCasts(int grenade, int ability1, int ability2, int ultimate) {
+    this.grenade = grenade;
+    this.ability1 = ability1;
+    this.ability2 = ability2;
+    this.ultimate = ultimate;
+  }
 }
